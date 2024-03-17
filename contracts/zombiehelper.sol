fd17f8771480e62e6bf293cd765ecd99b1f354da
Soham Kulkarni <soham7kulkarni@gmail.com>
2024-03-16 17:45:31 -0700

    Revert "added changeZombieName"
    This reverts commit 956f8d75815b26e10df560c097ef4a65d5f90f56.

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -31,11 +31,11 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].level = zombies[_zombieId].level.sub(1);
   }
 
-  function changeName(uint _zombieId, string _newName) external aboveLevel(1, _zombieId) onlyOwnerOf(_zombieId) {
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(1, _zombieId) onlyOwnerOf(_zombieId) {
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].dna = _newDna;
   }
 
